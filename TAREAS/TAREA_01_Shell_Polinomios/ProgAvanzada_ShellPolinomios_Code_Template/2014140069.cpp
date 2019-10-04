f3650c27d6212100e2dad9ff3f0f5843705935e7
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2019-10-04 00:08:16 -0500

    2019.10.04.00.07

---
+++ TAREAS/TAREA_01_Shell_Polinomios/ProgAvanzada_ShellPolinomios_Code_Template/2014140069.cpp
@@ -0,0 +1,36 @@
+/** 2014140069.cpp */
+#include "Directory_entry.h"
+#include "Pol_Directory.h"
+#include "Polinomio.h"
+#include "Rac.h"
+#include "String_Tokenizer.h"
+
+/** Agregar el c\'odigo necesario para guardar un polinomio en 
+ * el directorio de polinomios */
+//                                Viernes 3 de octubre de 2019
+//PARA: Discentes del grupo 2MV8                               
+//Despu\'es de la clase de este jueves de 16:00 a 17:30, revis\'e 
+//nuevamente el funcionamiento de la tarea 1 del curso y llegu\'e 
+//a la conclusi\'on de que el problema que se nos present\'o en 
+//la clase se debi\'o a que el archivo de recurso del proyecto 
+//"workspace.txt" se encontraba ``da\~nado''. Despu\'es de borrar 
+//el contenido de este archivo de texto y repetir las pruebas 
+//intentadas en clase se logr\'o almacenar correctamente nuevas 
+//variables polinomio. En virtud de lo anterior, la asignaci\'on 
+//del m\'etodo 
+//std::string 
+//add_or_change_entry(const std::string& var_name,
+//const std::string& coeffs);
+//a la discente con n\'umero de boleta 2014140069 queda sin 
+//materia de trabajo ya que ese m\'etodo a mi juicio est\'a 
+//ya codificado correctamente. Raz\'on por la cual, la 
+//asignaci\'on de la tarea 1 para la discente ser\'a el 
+//siguiente m\'etodo:
+#ifndef DISC2014140069
+
+#else
+// Discente 2014140069
+// Write your code here
+#endif /*DISC2014140069*/
+
+
