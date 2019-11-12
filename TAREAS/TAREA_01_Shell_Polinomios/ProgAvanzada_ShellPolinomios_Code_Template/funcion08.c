f6849c4433d0591b9d5d4a81ff09474f0a4552fc
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2019-11-12 17:29:58 -0600

    2019.11.12.17.29 funcion08.c funcion09.c funcion10.c

---
+++ TAREAS/TAREA_01_Shell_Polinomios/ProgAvanzada_ShellPolinomios_Code_Template/funcion08.c
@@ -0,0 +1,43 @@
+/**
+ * originalmente funcion08.c
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>     /*isatty()*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>     /*memset()*/
+//#define NDEBUG
+#include <assert.h>
+#include "Pol_Directory.h"
+#include "String_Tokenizer.h"
+#include "Polinomio.h"
+#define TAMDBUF       100
+
+using std::cout;
+#include <stdio.h>
+#include "Polinomio.h"
+#define DISC00
+#ifdef DISC00
+/*Stub*/
+/**
+ * @param command_line: un string que contendr\'a un comando 
+ *                      tecleado por el usuario (en el shell
+ *                      del programa) y devuelve true si se 
+ *                      est\'a tratando de ejecutar el comando
+ *                      composicion --que acordamos que ser\'ia
+ *                      de la forma comp(pol1,pol2). 
+ * */
+bool comp(std::string& command_line){
+    /* INCLUDE REMAINING CODE HERE */
+}
+#else
+bool comp(std::string& command_line){
+    /* INCLUDE REMAINING CODE HERE */
+}
+#endif
+
+
---
+++ TAREAS/TAREA_01_Shell_Polinomios/ProgAvanzada_ShellPolinomios_Code_Template/funcion09.c
@@ -0,0 +1,41 @@
+/**
+ * originalmente funcion09.c
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>     /*isatty()*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>     /*memset()*/
+//#define NDEBUG
+#include <assert.h>
+#include "Pol_Directory.h"
+#include "String_Tokenizer.h"
+#include "Polinomio.h"
+#define TAMDBUF       100
+
+using std::cout;
+#include <stdio.h>
+#include "Polinomio.h"
+#define DISC00
+#ifdef DISC00
+/*Stub*/
+void do_graficar(Pol_Directory& the_directory,
+                 std::string& pol2plot,
+                 float xMin,float xMax,float yMin,float yMax,
+                 int cantidadDPuntos){
+    /* INCLUDE REMAINING CODE HERE */
+}
+#else
+void do_graficar(Pol_Directory& the_directory,
+                 std::string& pol2plot,
+                 float xMin,float xMax,float yMin,float yMax,
+                 int cantidadDPuntos){
+    /* INCLUDE REMAINING CODE HERE */
+}
+#endif
+
+
