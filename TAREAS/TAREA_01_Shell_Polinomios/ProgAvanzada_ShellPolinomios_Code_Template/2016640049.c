5184618d2d28b7ed62de601a04e93e02ee70c5c3
caud9 <43735897+caud9@users.noreply.github.com>
2019-11-12 17:30:18 -0600

    Update 2016640049.c

--- TAREAS/TAREA_01_Shell_Polinomios/ProgAvanzada_ShellPolinomios_Code_Template/2016640049.c
+++ TAREAS/TAREA_01_Shell_Polinomios/ProgAvanzada_ShellPolinomios_Code_Template/2016640049.c
@@ -1,2 +1,48 @@
+/**
+ * originalmente funcion07.c
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
 #include <stdio.h>
-#include <
+#include <stdlib.h>
+#include <unistd.h>     /*isatty()*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>     /*memset()*/
+//#define NDEBUG
+#include <assert.h>
+#include "Pol_Directory.h"
+#include "String_Tokenizer.h"
+#include "Polinomio.h"
+#define TAMDBUF       100
+
+using std::cout;
+#include <stdio.h>
+#include "Polinomio.h"
+#define DISC00
+#ifdef DISC00
+/*Stub*/
+Polinomio composicion(Polinomio &pol1,Polinomio &pol2){
+    int sizePol1,sizePol2;
+    //ASIGNAR GRADO DE POLINOMIOS 
+    Polinomio ss;
+    int m
+    for(int i=0;i<sizePol1;i++) {
+        m=0
+        for(int j=0;j<sizePol1-m;j++){
+          
+        }
+        m++;
+        ss = suma(multi(pol1,pol1),ss);
+    }
+    Polinomio res;
+}
+#else
+void composicion(Pol_Directory& the_directory,
+                       string& op1,
+                       string& op2,
+                       string& var){
+    /* INCLUDE REMAINING CODE HERE */
+}
+#endif
