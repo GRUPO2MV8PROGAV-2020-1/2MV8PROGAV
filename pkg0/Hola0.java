10c88f95c50b340f2dbf86dd068015078a269575
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2019-11-19 16:37:54 -0600

    2019-11-19.16.37 Java Hello world!

---
+++ pkg0/Hola0.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0;
+
+/**
+ *
+ * @author LMC
+ * @version 2019.01.22
+ */
+public class Hola0 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo JAVA!");
+    }
+}
+class Hola1{
+    public static void main(String[] args) {
+        java.awt.Frame F=new java.awt.Frame("HOLA MUNDO JAVA");
+        F.setSize(400, 300);
+        F.setLocationRelativeTo(null);
+        F.setVisible(true);
+    }
+}
+
+
+
+
+
---
+++ pkg0/pkg0_1/Hola1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0.pkg0_1;
+import java.util.Scanner;
+//import java.awt.*;
+//import javax.swing.*;
+/**
+ *
+ * @author LMC
+ */
+public class Hola1 {
+    String S;
+    Scanner scan;
+    public String toString(){
+        String s=S;
+        return s;
+    }
+    public Hola1(){} //deliberadamente vacio
+    public Hola1(String str){
+        S=str;
+    }
+    public static void main(String[] args) {
+        Hola1 H1=new Hola1();
+        H1.S="GRUPO 2MV3";
+        System.out.println(H1+" Martes 22 de enero de 2019"+1);
+        H1=new Hola1("2MV3");
+        System.out.println(H1);
+    }
+}//end class pkg0.pkg0_1.Hola1
