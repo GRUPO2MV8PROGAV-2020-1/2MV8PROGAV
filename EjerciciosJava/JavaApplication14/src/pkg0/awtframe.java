3d2f6bea24936dc4be055c9f116039cd5c699a73
caud9 <erc.rock.b@gmail.com>
2019-11-26 17:25:35 -0600

    Java clase 26/11/2019

--- EjerciciosJava/JavaApplication14/src/pkg0/awtframe.java
+++ EjerciciosJava/JavaApplication14/src/pkg0/awtframe.java
@@ -11,8 +11,10 @@ package pkg0;
  */
 
 import java.awt.*;
+//import java.awt.Event.*;
+import java.awt.event.*; // 1/5
 
-public class awtframe {
+public class awtframe implements ActionListener{
     Frame F;
     Button B;
     TextArea TA;
@@ -24,14 +26,33 @@ public class awtframe {
         B = new Button("Add"); F.add(B);
         F.add(TF=new TextField("Write Here"));
         F.add(TA=new TextArea());
+        TA.setEditable(false);
+        F.addWindowListener(new WindowAdapter(){ // 2/5 3/5
+            public void windowClosing(WindowEvent we){// 4/5
+                F.dispose();// 5/5
+                System.exit(0);
+            }
+        });
         
-        F.setSize(400, 200);
+        B.addActionListener(this); // 3/5
+        
+        F.setSize(600, 200);
         F.setLocationRelativeTo(null);
         F.setVisible(true);
     }
     
     public static void main(String[] args) {
         new awtframe();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) { // 4/5
+        if(B.equals(ae.getSource())) { // 5/5
+            String txt0 = TA.getText();
+            String txt1 = TF.getText();
+            txt0 = txt0+txt1+'\n';
+            TA.setText(txt0);
+        }
     }
     
 }//end class awtframe
